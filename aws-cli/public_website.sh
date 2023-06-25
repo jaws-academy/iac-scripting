78560d5af974171b6e61792cc0950a6fe7269285
Peter Butkovic <peter.butkovic@sk.ibm.com>
2023-06-25 18:03:33 +0200

    env var usage fixed

--- aws-cli/public_website.sh
+++ aws-cli/public_website.sh
@@ -5,11 +5,11 @@ set -e
 UNIQUE_BUCKET_NAME=<UNIQUE_BUCKET_NAME>
 
 # Create a bucket + Enable static website hosting
-aws s3api create-bucket --bucket $UNIQUE_BUCKET_NAME --acl public-read
-aws s3 website s3://$UNIQUE_BUCKET_NAME --index-document index.html
+aws s3api create-bucket --bucket ${UNIQUE_BUCKET_NAME} --acl public-read
+aws s3 website s3://${UNIQUE_BUCKET_NAME} --index-document index.html
 
 # Add a bucket policy that makes your bucket content publicly available
-aws s3api put-bucket-policy --bucket $UNIQUE_BUCKET_NAME --policy file://policy_s3.json
+aws s3api put-bucket-policy --bucket ${UNIQUE_BUCKET_NAME} --policy file://policy_s3.json
 
 # dump an index document
 cat << EOF > index.html
@@ -25,9 +25,9 @@ cat << EOF > index.html
 EOF
 
 # upload index
-aws s3 cp index.html s3://$UNIQUE_BUCKET_NAME
+aws s3 cp index.html s3://${UNIQUE_BUCKET_NAME}
 
 
 
 # Test your website endpoint
-curl http://$UNIQUE_BUCKET_NAME.s3-website.us-east-1.amazonaws.com
+curl http://${UNIQUE_BUCKET_NAME}.s3-website.us-east-1.amazonaws.com
